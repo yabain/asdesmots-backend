bcc991a92d5f792b4483ae54d679718dec8f0c57
Cedric-Yaba-In <c.nguendap@yabain.com>
2023-04-22 10:33:07 +0200

    change jenkinsfile

--- src/game/controllers/game-arcarde.controller.ts
+++ src/game/controllers/game-arcarde.controller.ts
@@ -22,8 +22,16 @@ export class GameArcardeController
      * @apiSuccess (201 Created) {Number} statusCode status code
      * @apiSuccess (201 Created) {String} Response Description
      * @apiSuccess (201 Created) {Object} data response data
-     * @apiSuccess (201 Created) {String} data.name Role name
-     * @apiSuccess (201 Created) {String} data.description Role description
+     * @apiSuccess (201 Created) {String} data.name Game arcarde name
+     * @apiSuccess (201 Created) {String} data.description Game arcarde description
+     * @apiSuccess (201 Created) {Boolean} data.isOnlineGame is set to true if the game is online and false otherwise
+     * @apiSuccess (201 Created) {Boolean} data.canRegisterPlayer Is set to true if players can register or not
+     * @apiSuccess (201 Created) {Boolean} data.isFreeRegistrationPlayer Is set to true if the participation in the games is free or not
+     * @apiSuccess (201 Created) {Number} data.maxPlayersNumber  Maximum number of player
+     * @apiSuccess (201 Created) {Date} data.startDate game start date
+     * @apiSuccess (201 Created) {Date} data.endDate game end date
+     * @apiSuccess (201 Created) {Date} data.startRegistrationDate game registration start date
+     * @apiSuccess (201 Created) {Date} data.endRegistrationDate game registration end date
      * 
      * @apiError (Error 4xx) 401-Unauthorized Token not supplied/invalid token 
      * @apiUse apiError
--- src/game/dtos/update-game-arcarde.dto.ts
+++ src/game/dtos/update-game-arcarde.dto.ts
@@ -2,6 +2,4 @@ import { PartialType } from "@nestjs/mapped-types";
 import { CreateGameArcardeDTO } from "./create-game-arcarde.dto";
 
 export class UpdateGameArcarde  extends PartialType(CreateGameArcardeDTO)
-{
-    
-}
+{}
