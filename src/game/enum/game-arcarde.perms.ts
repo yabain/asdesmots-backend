c233dd76faa87e99f2d14f9b4d5e6a32e9640efa
Cedric-Yaba-In <c.nguendap@yabain.com>
2023-04-25 07:56:15 +0200

    add url for get competition game by id

--- src/game/enum/game-arcarde.perms.ts
+++ src/game/enum/game-arcarde.perms.ts
@@ -6,6 +6,11 @@ export const GameArcardePerms: PermsType = {
         description:"List of game arcard",
         module: "GAME_ARCARDE"
     },
+    READ_OWNER : {
+        name:"gamearcarde.read.owner",
+        description:"Get game arcard",
+        module: "GAME_ARCARDE"
+    },
     CREATE : {
         name: "gamearcarde.create",
         description:"Create a new game arcarde",
