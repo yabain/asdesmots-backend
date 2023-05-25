80a51fc553d2c600b2f3cef29aac0c2c027731f2
Cedric-Yaba-In <c.nguendap@yabain.com>
2023-05-25 09:11:41 +0200

    update email template

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  version = "1.3.0"
+  version = "1.3.1"
   constructor(private configService:ConfigService) {}
 
   @Get()
