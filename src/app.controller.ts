65d678db96d8d223f944e5d60f9dd9c7f9ae0a9c
Cedric-Yaba-In <c.nguendap@yabain.com>
2023-07-26 22:54:55 +0200

    update version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   
-  version = "1.3.2.0"
+  version = "1.3.2.1"
 
   constructor(private configService:ConfigService) {}
 
