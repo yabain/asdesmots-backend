ac3d1a53375ab44755b97ade12821e7871adb362
Chrispin7 <134141828+Chrispin7@users.noreply.github.com>
2023-11-03 17:48:12 +0100

    Add files via upload

--- assign-user-role.dto.ts
+++ assign-user-role.dto.ts
@@ -5,7 +5,7 @@ import { ObjectId } from "mongoose";
  * Définition de la documentation du model app
  * @apiDefine AddRoleUserDTO Body of the request to assign a role to a user
  * @apiBody {String} userId User ID
- * @apiBody {String} roleId Role ID
+ * @apiBody {ObjectId[]} roleId Role ID
  */
 
 export class AssignUserRoleDTO
